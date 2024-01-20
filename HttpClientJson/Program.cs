a8a03bc14bdc5f153af83c57f18369bce9ee482a
moham <moham@DESKTOP-JECN4I9>
2024-01-20 16:48:10 +0200

    Add project files.

---
+++ HttpClientJson/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+namespace HttpClientJosn
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (HttpClient client = new ())
+            {
+                string todoJsonContent = await client.GetStringAsync("https://jsonplaceholder.typicode.com/todos");
+                List<Todo>? todosList = JsonSerializer.Deserialize<List<Todo>>(todoJsonContent, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                foreach (Todo item in todosList!)
+                {
+                   Console.WriteLine(item);
+                }
+                
+            }
+            Console.ReadKey();
+        }
+
+    }
+
+    // يستحسن عدم التعديل على الخصائص لكن لجمال الكود تم التعديل علي او حرف وجعله كبير وفى المقابل تم إضافة الخاصية
+    // , new JsonSerializerOptions { PropertyNameCaseInsensitive=true} للتجاوز ذلك
+    public class Todo
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+        public override string ToString()
+        {
+            return $"\n [{Id} - UserId: {UserId}] -  {Title} {(Completed ? "completed" : "not completed")}";
+        }
+    }
+
+}
---
+++ XMLSerialization/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XMLSerialization01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new()
+            {
+                Id = 1001,
+                Fname = "Issam",
+                Lname = "A.",
+                Benefits = { "Pension", "Health Insurance" }
+            };
+
+            string xmlContent = SerializationToXml(e1);
+            File.WriteAllText("XmlData.xml", contents:  xmlContent);
+            //Console.WriteLine(xmlContent);
+
+            Employee e2 = DeserializedToObject(xmlContent);
+            Console.WriteLine(e2.Fname);
+
+            Console.ReadKey();
+        }
+
+        private static Employee DeserializedToObject(string xmlContent)
+        {
+            Employee? e;
+            XmlSerializer serializer = new(typeof(Employee));
+            using(TextReader  reader = new StringReader(xmlContent))
+            {
+                e = serializer.Deserialize(reader) as Employee;
+            }
+            return e!;
+        }
+
+        private static string SerializationToXml(Employee e1)
+        {
+            string reslut;
+            XmlSerializer serializer = new(typeof(Employee)); // or e.GetType()
+            using(StringWriter sw = new())
+            {
+                using(XmlWriter w = XmlWriter.Create(sw, new XmlWriterSettings { Indent= true}))
+                {
+                    serializer.Serialize(w, e1);
+                    reslut = sw.ToString()!;
+                }
+            }
+            return reslut;
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public List<string> Benefits { get; set; } = new List<string>();
+    }
+}
